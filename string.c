5d76fc175ab62fba2b5dfc9f315f954fdd1b9c1b
gyujun9403 <gujun112@naver.com>
2021-03-16 00:32:47 +0900

    0316 BSQ

--- string.c
+++ string.c
@@ -6,8 +6,33 @@
 /*   By: gyeon <gyeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 00:14:30 by gyeon             #+#    #+#             */
-/*   Updated: 2021/03/16 00:14:36 by gyeon            ###   ########.fr       */
+/*   Updated: 2021/03/16 00:32:09 by gyeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
 
+	i = 0;
+	while (*dest && i < size)
+	{
+		++i;
+		++dest;
+	}
+	while (*src && i + 1 < size)
+	{
+		*dest = *src;
+		++dest;
+		++src;
+		++i;
+	}
+	if (i < size)
+		*dest = 0;
+	while (*src)
+	{
+		++i;
+		++src;
+	}
+	return (i);
+}
